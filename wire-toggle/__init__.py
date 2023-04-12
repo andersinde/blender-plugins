6015ea0a479c2ae792502a8f3b752202d301250d
Anders Inde <anders.inde@assaabloy.com>
2023-04-12 18:46:14 +0200

    add wire-toggle operator

---
+++ wire-toggle/__init__.py
@@ -0,0 +1,26 @@
+import bpy
+
+class WireToggleOperator(bpy.types.Operator):
+    bl_idname = "object.wire_toggle"
+    bl_label = "Toggle wireframe display"
+
+    @classmethod
+    def poll(cls, context):
+        return context.active_object is not None
+
+    def execute(self, context):
+        if bpy.context.object.display_type == 'SOLID':
+            bpy.context.object.display_type = 'WIRE'
+        elif bpy.context.object.display_type == 'WIRE':
+            bpy.context.object.display_type = 'SOLID'
+        return {'FINISHED'}
+
+def register():
+    bpy.utils.register_class(WireToggleOperator)
+
+def unregister():
+    bpy.utils.unregister_class(WireToggleOperator)
+
+if __name__ == "__main__":
+    register()
+
